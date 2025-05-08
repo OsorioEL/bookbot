60975b399677c803db742bbe6c2b7ccb54104976
OsorioEL <ericlosorio@gmail.com>
2025-05-08 02:32:03 -0400

    added sys import in order to use sys.argv in order to run program with multiple books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys 
 from stats import get_num_words, get_char_count, sorted_dict
 
 def get_book_text(file_path):
@@ -6,20 +7,26 @@ def get_book_text(file_path):
   return text
 
 def main():
-  file_path = 'books/frankenstein.txt'
-  book_text = get_book_text(file_path)
-  # print(f"{get_num_words(book_text)} words found in the document.")
-  # print(f"dictionary of characters and their counts: {get_char_count(book_text)}")
-  print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
-  print("----------- Word Count ----------")
-  print(f"Found {get_num_words(book_text)} total words")
-  print("----------- Character Count -----------")
-  sorted_dict_list = sorted_dict(get_char_count(book_text))
-  for d in sorted_dict_list:
-    if d["char"].isalpha():
-      print(f"{d["char"]}: {d["num"]}")
-  print("============= END ===============")
+  # Check if the file path is provided as a command line argument
+  if len(sys.argv) > 1:
+    file_path = sys.argv[1]
+    book_text = get_book_text(file_path)
+    # print(f"{get_num_words(book_text)} words found in the document.")
+    # print(f"dictionary of characters and their counts: {get_char_count(book_text)}")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book_text)} total words")
+    print("----------- Character Count -----------")
+    sorted_dict_list = sorted_dict(get_char_count(book_text))
+    for d in sorted_dict_list:
+      if d["char"].isalpha():
+        print(f"{d["char"]}: {d["num"]}")
+    print("============= END ===============")
+  else:
+    print("Please provide a file path as a command line argument.")
+    print("Usage: python3 main.py <file_path>")
+    sys.exit(1)
   
 if __name__ == "__main__":
   main()
